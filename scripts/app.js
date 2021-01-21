c99a86d25debd186b95c11038f2f701ae7a985d1
Lia Sexton <liamaggiorotto@gmail.com>
2021-01-21 10:53:49 -0600

    breaks original navbar. reconfiguring to open from side.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,7 @@
 console.log("Sanity Check");
+
+$(document).ready(function () {
+    $('[data-toggle=collapse-side]').click(function (e) {
+        $('.side-collapse').toggleClass('in');
+    });
+});
