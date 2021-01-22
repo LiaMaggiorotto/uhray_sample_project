df2c213648b1dadfadd83bd74dafdf1603c315bb
Lia Sexton <liamaggiorotto@gmail.com>
2021-01-22 10:48:34 -0600

    removes nav bar custom jquery

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,1 @@
 console.log("Sanity Check");
-
-// $(document).ready(function () {
-//     $('[data-toggle=collapse-side]').click(function (e) {
-//         $('.side-collapse').toggleClass('in');
-//     });
-// });
